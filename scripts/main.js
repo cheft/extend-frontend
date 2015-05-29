2dcd9e772c856568d1b0bb1ed5481ef3f6171398
unknown <100348@PC100348.dafycredit.com>
2015-05-29 15:06:17 +0800

    require riot

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
+window.riot = require('riot');
+window.c = require('../assets/js/cheft');
+
 require('./app/viewport/tag');
 require('./app/ranking/tag');
 require('./app/share/tag');
 require('./app/footer/tag');
 require('./app/test/tag');
 
-window.app = new Cheft.Application();
+window.app = new c.Application();
 app.mount('viewport');
 
-app.router = new Cheft.Router(require('./router'));
+app.router = new c.Router(require('./router'));
 app.router.start();
