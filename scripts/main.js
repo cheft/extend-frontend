650843817a46b90cf9885e806266189c71ff27bb
unknown <100348@PC100348.dafycredit.com>
2015-06-02 17:01:22 +0800

    推荐有你静态页面提交

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ require('./app/ranking/tag');
 require('./app/invitation/tag');
 require('./app/share/tag');
 require('./app/footer/tag');
+require('./app/activity/tag');
 
 window.app = new c.Application({urlRoot: 'http://10.10.51.118:3000/'});
 app.mount('viewport');
