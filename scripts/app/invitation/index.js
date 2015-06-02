741376b894f0c079af5575e7eb14fe29f568a093
陈海峰 <cn.cheft@gmail.com>
2015-06-02 09:35:16 +0800

    add icon

--- scripts/app/invitation/index.js
+++ scripts/app/invitation/index.js
@@ -6,9 +6,18 @@ module.exports = {
     events: {
         mount: function() {
             self = this;
+            var l = $.loading({
+                content:'加载数据',
+            });
             this.ranking.get().done(function(data) {
                 self.list = data;
                 self.update();
+                l.loading('hide');
+                $.tips({
+                    content:'数据加载完成',
+                    stayTime:2000,
+                    type: 'success'
+                })
             });
         } 
     }
