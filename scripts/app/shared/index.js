7bd58d87a787775d4e6f5ed8eb815719344cd3b5
张兵涛 <krui0728@126.com>
2015-06-04 17:50:57 +0800

    shared modifiy

--- scripts/app/shared/index.js
+++ scripts/app/shared/index.js
@@ -3,7 +3,7 @@ module.exports = {
         mount: function() {
             var prizeHeight = ($(document).height() > $(window).height()) ? 130 : ($(window).height() - $(".c-head").height() - $(".c-person-info").height() - $(".c-background-color").height()); 
             var prizeMt = (prizeHeight - 130)/2;
-            $(".c-prize").css("margin-top",prizeMt);
+            $(".c-prize").css("margin-top",prizeMt-1);
             $(".c-prize").css("margin-bottom",prizeMt);
         }
     }
