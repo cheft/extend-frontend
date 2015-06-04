fcc9cca3a3ea3b6e61a64676b6ec261ef9f282eb
张兵涛 <krui0728@126.com>
2015-06-04 15:17:34 +0800

    footer固定在底部，用到footer的页面都添加c-container类

--- scripts/app/about/index.js
+++ scripts/app/about/index.js
@@ -1,10 +1,8 @@
 module.exports = {
     events: {
         mount: function() {
-            alert(111);
-            var containerHeight = ($("document").height() > $("window").height()) ? $("document").height(): $("window").height(); 
-            alert(222);
-            alert(containerHeight);
+            var containerHeight = ($(document).height() > $(window).height()) ? $(document).height(): ($(window).height()-$(".c-footer").height()); 
+            $(".c-container").css("height",containerHeight);
         } 
     }
 }
