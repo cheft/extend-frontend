626a0fb7f174d055ae75ee784efe7de92c706467
张兵涛 <krui0728@126.com>
2015-06-17 14:43:08 +0800

    about introduction detail

--- scripts/app/about/index.js
+++ scripts/app/about/index.js
@@ -1,22 +1,29 @@
 module.exports = {
+    actions: {
+        problem: function(e) {
+            app.router.go('problem');
+        },
+        aboutUs: function(e) {
+            app.router.go('about-us');
+        },
+        contactUs: function(e) {
+            var dialog = $('.ui-dialog');
+            dialog.dialog('show');
+            var del = $('#del-layer');
+            del.on('touchstart',function(){
+                dialog.dialog('hide');
+            });
+            var tel = $('#tel-bt');
+            tel.on('touchstart',function(){
+                location.href = 'tel:40080075546';
+            });
+        }
+    },
+
     events: {
         mount: function() {
             var containerHeight = ($(document).height() > $(window).height()) ? $(document).height(): ($(window).height()-$(".c-footer").height());
-            $(".c-container").css("height",containerHeight);
-            
-            var contact = $("#contact-us");
-            contact.on("touchstart",function(){
-            	var dialog = $('.ui-dialog');
-				dialog.dialog('show');
-				var del = $('#del-layer');
-				del.on('touchstart',function(){
-					dialog.dialog('hide');
-				});q
-				var tel = $('#tel-bt');
-				tel.on('touchstart',function(){
-					location.href = 'tel:40080075546';
-				});
-            });    
+            $('.c-container').css('height',containerHeight);               
         }
     }
 }
