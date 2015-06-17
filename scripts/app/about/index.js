75a2604d2f682c5164f28a1810cad47addd0f7af
张兵涛 <krui0728@126.com>
2015-06-17 11:37:11 +0800

    add aboutme

--- scripts/app/about/index.js
+++ scripts/app/about/index.js
@@ -3,6 +3,20 @@ module.exports = {
         mount: function() {
             var containerHeight = ($(document).height() > $(window).height()) ? $(document).height(): ($(window).height()-$(".c-footer").height());
             $(".c-container").css("height",containerHeight);
+            
+            var contact = $("#contact-us");
+            contact.on("touchstart",function(){
+            	var dialog = $('.ui-dialog');
+				dialog.dialog('show');
+				var del = $('#del-layer');
+				del.on('touchstart',function(){
+					dialog.dialog('hide');
+				});q
+				var tel = $('#tel-bt');
+				tel.on('touchstart',function(){
+					location.href = 'tel:40080075546';
+				});
+            });    
         }
     }
 }
